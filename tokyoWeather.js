18f9381dd1fd5e57ac930765c59de60f3eb69e16
Raymond SO <rso@ouhk.edu.hk>
2015-09-23 09:53:23 +0800

    Added tokyoWeather.js - a simple example of displaying json data received from a web service

---
+++ tokyoWeather.js
@@ -0,0 +1,28 @@
+var http = require('http');
+
+var options = {
+    host: 'api.openweathermap.org',
+    port: 80,
+    path: '/data/2.5/weather?q=Tokyo,jp&units=metric',
+    method: 'GET'
+};
+
+var temperature = 0.0;
+
+var req = http.request(options, function(res) {
+    res.setEncoding('utf8');
+
+    res.on('data', function (chunk) {
+        var jsonObj = JSON.parse(chunk);
+        console.log("Current Temp. : " + jsonObj.main.temp);
+        console.log("Max Temp : "      + jsonObj.main.temp_max);
+        console.log("Min Temp : "      + jsonObj.main.temp_min);
+    });
+});
+
+req.on('error', function(e) {
+    console.log('Problem with request: ' + e.message);
+});
+
+req.end();
+
