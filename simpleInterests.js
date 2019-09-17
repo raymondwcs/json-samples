96e4ccc0023072703d4660c8a62330e4d91aaa82
Raymond SO <rso@ouhk.edu.hk>
2019-09-17 10:51:56 +0800

    added default parameter values

--- simpleInterests.js
+++ simpleInterests.js
@@ -6,8 +6,12 @@ function SimpleInterests() {
 	this.interests = this.principal * this.rate * this.period;
 }
 */
+const principal = 1000000;
+const rate = 0.01;
+const period = 10;
+
 class SimpleInterests {
-	constructor(p=1000000,r=0.01,t=10) {
+	constructor(p=principal,r=rate,t=period) {
 		this.principal = p;
 		this.rate = r;
 		this.period = t;
@@ -23,5 +27,11 @@ ${this.rate *100}% for ${this.period} years = $${this.interests}`);
 let obj1 = new SimpleInterests();
 obj1.print();
 
-let obj2 = new SimpleInterests(10000,0.03,10);
+let obj2 = new SimpleInterests(10000);
 obj2.print();
+
+let obj3 = new SimpleInterests(10000,0.03);
+obj3.print();
+
+let obj4 = new SimpleInterests(10000,0.03,10);
+obj4.print();
