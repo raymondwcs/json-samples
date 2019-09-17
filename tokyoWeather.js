552df53cf898e8d03bcb3b7e0e638f09d8977a63
Raymond SO <rso@ouhk.edu.hk>
2019-09-17 08:02:28 +0800

    updated

--- tokyoWeather.js
+++ tokyoWeather.js
@@ -1,28 +1,24 @@
-var http = require('http');
-
-var APIKEY="";       //**your API key***
-
-var options = {
+const http = require('http');
+const APIKEY='';       //**your API key***
+const options = {
     host: 'api.openweathermap.org',
     port: 80,
     path: '/data/2.5/weather?q=Tokyo,jp&units=metric&APPID=' + APIKEY,
     method: 'GET'
 };
 
-var req = http.request(options, function(res) {
+const req = http.request(options, (res) => {
     res.setEncoding('utf8');
 
-    res.on('data', function (chunk) {
-        var jsonObj = JSON.parse(chunk);
+    res.on('data', (chunk) => {
+        var jsonObj = JSON.parse(chunk); // convert to JSON
         console.log(chunk);
-        console.log("Current Temp. : " + jsonObj.main.temp);
-        console.log("Max Temp : "      + jsonObj.main.temp_max);
-        console.log("Min Temp : "      + jsonObj.main.temp_min);
+		  console.log(`Tokyo is located at [${jsonObj.coord.lon},${jsonObj.coord.lat}]`);
     });
 });
 
-req.on('error', function(e) {
-    console.log('Problem with request: ' + e.message);
+req.on('error', (error) => {
+    console.log(`Problem with request: ${error.message}`);
 });
 
 req.end();
