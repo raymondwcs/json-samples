200b0aae9651b004c59224918b6c01fad1a2a514
Raymond SO <rso@ouhk.edu.hk>
2019-09-25 19:54:21 +0800

    initial version

---
+++ shape.js
@@ -0,0 +1,36 @@
+class Shape {
+    constructor() {
+        this.X = 0;
+        this.Y = 0;
+    }
+    move(x,y) {
+        this.X = x;
+        this.Y = y;
+    }
+    distance_from_origin() {
+        return Math.sqrt(this.X*this.X + this.Y*this.Y);
+    }
+}
+
+let s = new Shape();
+s.move(10,10);
+console.log(s.distance_from_origin());
+
+class Square extends Shape {
+    constructor(width = 5) {
+        super();
+        this.width = width;
+    }
+    area() {
+        return(this.width * this.width);
+    }
+}
+
+let sq = new Square();
+sq.move(-5,-5);
+sq.width = 5;
+console.log(sq.distance_from_origin());
+
+console.log(sq instanceof Square);
+console.log(sq instanceof Shape);
+console.log(sq instanceof Date);
